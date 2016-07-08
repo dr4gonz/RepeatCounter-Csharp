0c08b89f8363ffbfff2478f8f9cc3157642c3241
Matthew Reyes <mreyez@gmail.com>
2016-07-08 08:22:05 -0700

    add code for second test to input word + char and get num of times character is repeated

--- Tests/RepeatCounterTests.cs
+++ Tests/RepeatCounterTests.cs
@@ -6,11 +6,13 @@ namespace RepeatCounter.Objects
 {
     public class RepeatCounterTests
     {
+        [Fact]
         public void EnterStringOfCharactersAndSpecificCharacter_Return_NumOfTimesItAppears()
         {
             RepeatCounter newCounter = new RepeatCounter();
             Assert.Equal(6, newCounter.CountRepeats("aaaaasddfsdfgjkgjkjhgkja","a"));
         }
+        [Fact]
         public void EnterWordAndSpecificCharacter_Return_NumOfTimesItAppears()
         {
             RepeatCounter newCounter = new RepeatCounter();
